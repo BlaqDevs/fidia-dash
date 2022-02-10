68ea0a5b489aa14d31267ba3a097abeec50710c1
BlaqDevs <adebomor@gmail.com>
2022-02-10 12:46:28 +0100

    layout

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <NavBar />
+    <v-main>
+      <v-container-fluid>
+        <Nuxt />
+      </v-container-fluid>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
